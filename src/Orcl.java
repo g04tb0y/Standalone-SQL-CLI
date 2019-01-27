2bcbed2880cdf3f8c84793eb81bb0f6520cd3947
g04tb0y <toolshed0x90@gmail.com>
2019-01-27 16:01:35 +0100

    minor fix for compatibility with Java 1.6

--- src/Orcl.java
+++ src/Orcl.java
@@ -4,42 +4,39 @@ import java.sql.*;
 
 public class Orcl {
 
-        public static void main(String[] args) {
+        public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
             if (args.length == 5) {
                 System.out.println("Echo params: ");
-                Arrays.stream(args).forEach(System.out::println);
-
-                try {
-                    Class.forName("oracle.jdbc.driver.OracleDriver");
-                    String connectionString = "jdbc:oracle:thin:@//" + args[0] + ":" + args[1];
-
-                    //Get connection using driver manager
-                    Connection connection = DriverManager.getConnection(connectionString, args[2], args[3]);
-                    if (connection != null) {
-                        System.out.println("Connection OK, we are in!");
-                    } else {
-                        System.out.println("Failed to create connection...");
-                        return;
-                    }
-                    Statement stm = connection.createStatement();
-                    ResultSet res = stm.executeQuery(args[4]);
-                    ResultSetMetaData rmd = res.getMetaData();
-
-                    while(res.next()) {
-                        System.out.println(res.getRow());
-                        for (int i = 1; i <= rmd.getColumnCount(); i++) {
-                            if (i > 1) System.out.print(",  ");
-                            System.out.print(rmd.getColumnName(i) + ": " + res.getString(i));
-                        }
-                        System.out.println();
-                    }
-
-                    connection.close();
-
-                } catch (SQLException | ClassNotFoundException e) {
-                    e.printStackTrace();
+                for(int i = 0; i < args.length;i++){
+                    System.out.println(args[i]);
                 }
+
+                Class.forName("oracle.jdbc.driver.OracleDriver");
+                String connectionString = "jdbc:oracle:thin:@//" + args[0] + ":" + args[1];
+
+                //Get connection using driver manager
+                Connection connection = DriverManager.getConnection(connectionString, args[2], args[3]);
+                if (connection != null) {
+                    System.out.println("Connection OK, we are in!");
+                } else {
+                    System.out.println("Failed to create connection...");
+                    return;
+                }
+                Statement stm = connection.createStatement();
+                ResultSet res = stm.executeQuery(args[4]);
+                ResultSetMetaData rmd = res.getMetaData();
+
+                while(res.next()) {
+                    System.out.println(res.getRow());
+                    for (int i = 1; i <= rmd.getColumnCount(); i++) {
+                        if (i > 1) System.out.print(",  ");
+                        System.out.print(rmd.getColumnName(i) + ": " + res.getString(i));
+                    }
+                    System.out.println();
+                }
+
+                connection.close();
 
             }
             else {
